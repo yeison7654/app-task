ee1a0655519223fc321e856faad24e3dd9ecf557
yeison7654 <yeisoncarhuapoma@gmail.com>
2024-10-31 16:07:22 -0500

    falta construir el modal de salir sesion

--- app/assets/js/functions_home.js
+++ app/assets/js/functions_home.js
@@ -1,15 +1,32 @@
+let loadingScreen = document.querySelector('.loading-screen');
 document.addEventListener("DOMContentLoaded", function () {
-
     setTimeout(() => {
         closeModal();
+        openModalSaveTask();
+        loadingScreen.classList.add("hidden");
     }, 1000);
+
 });
 
+/*
+ * Funcion que cierra el modal
+ */
 function closeModal() {
     let arrData = document.querySelectorAll('.btn-close-modal');
     arrData.forEach((element) => {
         element.addEventListener("click", () => {
-            alert("cerrar ventana");
+            let modal = document.querySelector('.modal');
+            modal.classList.add("hidden");
         });
     })
+}
+/*
+ *Funcion que abre el modal para registrar nuevas tareas
+ */
+function openModalSaveTask() {
+    let btnFormRegisterTask = document.getElementById('btn-form-register');
+    btnFormRegisterTask.addEventListener("click", function () {
+        let modalSaveTask = document.querySelector('.modal-save-task');
+        modalSaveTask.classList.remove("hidden");
+    });
 }
