b1729a5d5da872670cef8951a820ebb45f4ac9a9
yeison7654 <yeisoncarhuapoma@gmail.com>
2024-11-05 15:09:39 -0500

    app task

---
+++ app/assets/js/alert.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    viewAlert('error', 'Tarea registrada correctamente');
+});
+
+function viewAlert(type, message) {
+    const alertContainer = document.querySelector('.alert-container');
+    const alert = document.createElement('div');
+    alert.classList.add('custom-alert', `alert-${type}`)
+    alert.textContent = message;
+    alertContainer.appendChild(alert);
+    setTimeout(() => {
+        alert.remove();
+    }, 4000);
+}
