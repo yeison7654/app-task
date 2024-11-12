7ad46d3a999501793e733752255ca2a3d83678ca
yeison7654 <yeisoncarhuapoma@gmail.com>
2024-11-12 14:59:30 -0500

    mysql preparado

--- models/mysql.php
+++ models/mysql.php
@@ -9,9 +9,55 @@ class Mysql extends Conexion
         $this->conexion = new Conexion();
         $this->conexion = $this->conexion->getConexion();
     }
+    /*
+     * Esta funcion me permite realizar registros de tipo inserte en la base de datos
+     */
     protected function insert($sql, array $data)
     {
-        $request = $this->conexion->prepare($sql);
-        $request->execute($data);
+        $prepared = $this->conexion->prepare($sql);
+        $prepared->execute($data);
+        $request = $this->conexion->lastInsertId();
+        return $request;
     }
+    /*
+     *Esta funcion permite que realice consulta de tipo select en la base de datos
+     * donde devolvera un solo registro
+     */
+    protected function select($sql, array $data)
+    {
+        $prepared = $this->conexion->prepare($sql);
+        $prepared->execute($data);
+        $request = $prepared->fetch(PDO::FETCH_ASSOC);
+        return $request;
+    }
+    /*
+     * Esta funcion me permite actualizar registros de tipo inserte en la base de datos
+     */
+    protected function update($sql, array $data)
+    {
+        $prepared = $this->conexion->prepare($sql);
+        $request = $prepared->execute($data);
+        return $request;
+    }
+    /*
+     * Esta funcion me permite eleiminar registros de tipo inserte en la base de datos
+     */
+    protected function delete($sql, array $data)
+    {
+        $prepared = $this->conexion->prepare($sql);
+        $request = $prepared->execute($data);
+        return $request;
+    }
+    /*
+     *Esta funcion permite que realice consulta de tipo select en la base de datos
+     * donde devolvera todos los registros
+     */
+    protected function selectALL($sql, array $data)
+    {
+        $prepared = $this->conexion->prepare($sql);
+        $prepared->execute($data);
+        $request = $prepared->fetchAll(PDO::FETCH_ASSOC);
+        return $request;
+    }
+
 }
