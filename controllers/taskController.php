a142b7bb0e5fbd5636933b792847aca016dd7731
yeison7654 <yeisoncarhuapoma@gmail.com>
2024-12-03 14:35:41 -0500

    create task

---
+++ controllers/taskController.php
@@ -0,0 +1,38 @@
+<?php
+require_once '../models/task.php';
+class TaskController extends Task
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function createTask( $title, $description, $date, $hour){
+        session_start($this->nameSesion());
+        //llenado de variables
+        $idUser= $_SESSION["user_info"]["id"];
+        $status="pendiente";
+        if($title=="" || $date=="" || $hour==""){
+            echo $this->toJson(
+                'status' => false,
+                'message' => "Los campos no pueden estar vacios",
+                'type' => 'error'
+            );  
+            die();          
+        }
+        $request=$this->createTask($idUser, $title, $description, $status, $date, $hour);
+        if($request>0){
+            echo $this->toJson(
+                'status' => true,
+                'message' => "Tarea registrada correctamente",
+                'type' => 'success'
+            );  
+            die();    
+        }
+        echo $this->toJson(
+            'status' => false,
+            'message' => "Ocurrio un error y no logro registrar la tarea",
+            'type' => 'error'
+        );  
+        die();
+    }
+}
---
+++ models/task.php
@@ -0,0 +1,33 @@
+<?php
+require_once 'mysql.php';
+class Task extends Mysql
+{
+    private $id;
+    private $idUser;
+    private $title;
+    private $description;
+    private $status;
+    private $date;
+    private $hour;
+    protected function __construct()
+    {
+        parent::__construct();
+    }
+    /**
+     * Crear o registrar la tarea
+     */
+    protected function createTask($idUser, $title, $description, $status, $date, $hour)
+    {
+        $data = array(
+            $this->idUser = $idUser,
+            $this->title = $title,
+            $this->description = $description,
+            $this->date = $date,
+            $this->hour = $hour,
+            $this->status = $status,
+        );
+        $sql = "INSERT INTO tareas (usuario_id,titulo,descripcion,fecha,hora,estado) VALUE (?,?,?,?,?,?);";
+        $response = $this->insert($sql, $data);
+        return $response;
+    }
+}
